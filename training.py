e146f301db323c949d26cf63015b3cee46e38660
IsuraNimalasri <isuranimalasiri@gmail.com>
2022-11-21 23:06:52 +0000

    Traning Steps

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+from json.tool import main
 from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
@@ -6,7 +7,10 @@ import os
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder
 import json
+from joblib import dump
+import prepocessing as pp
 
 ###################Load config.json and get path variables
 with open('config.json','r') as f:
@@ -16,17 +20,27 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 model_path = os.path.join(config['output_model_path']) 
 
 
+
 #################Function for training the model
 def train_model():
     
+    df = pd.read_csv(os.path.join(dataset_csv_path, "finaldata.csv"))
+    df_x, df_y, encoder = pp.preprocess_data(df, None)
+    x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.20)
+    
+    
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+    _model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+            intercept_scaling=1, l1_ratio=None, max_iter=100,
+            multi_class='ovr', n_jobs=None, penalty='l2',
+            random_state=0, solver='liblinear', tol=0.0001, verbose=0,
+            warm_start=False)
     
     #fit the logistic regression to your data
-    
+    _model.fit(x_train, y_train)
     #write the trained model to your workspace in a file called trainedmodel.pkl
-
+    dump(_model, os.path.join(model_path, "trainedmodel.pkl"))
+    dump(encoder, os.path.join(model_path, "encoder.pkl"))
+    
+if __name__ == "__main__":
+    train_model()
