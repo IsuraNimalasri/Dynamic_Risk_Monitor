da3fbedaf9f280af7aa748cf7bcf1c4da3dec5d3
IsuraNimalasri <isuranimalasiri@gmail.com>
2022-11-21 23:18:18 +0000

    Add dignositics steps

--- deployment.py
+++ deployment.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
+from shutil import copy2
 
 
 ##################Load config.json and correct path variable
@@ -16,12 +17,25 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
+output_folder_path = config["output_folder_path"]
+model_path = os.path.join(config['output_model_path']) 
 
 ####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
+def store_model_into_pickle():
+    
+    for f in [
+        "ingestedfiles.txt", "trainedmodel.pkl", "encoder.pkl", "latestscore.txt", ]:
+        if f in ["ingestedfiles.txt"]:
+            source_filepath = os.path.join(output_folder_path, f)
+        else:
+            source_filepath = os.path.join(model_path, f)
+        new_filepath = os.path.join(prod_deployment_path, f)
+        print(f'Copying {source_filepath} to {new_filepath}')
+        copy2(source_filepath, new_filepath)
+
+
+if __name__ == '__main__':
+    store_model_into_pickle()   
         
         
 
